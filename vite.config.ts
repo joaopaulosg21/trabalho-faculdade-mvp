8bd410aff83e52265f5ccc55468d6e57e8f2dd1b
João Paulo <joaopaulosg17@gmail.com>
2025-11-26 10:10:04 -0300

    updated config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/",
+  base: "/trabalho-faculdade-mvp/",
   server: {
     host: "::",
     port: 8080,
